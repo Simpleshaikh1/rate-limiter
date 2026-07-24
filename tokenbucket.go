2f6bd17e0f8b73485f16b0d476ea5bfba3cd4cd0
Toyyib <hamzatbolaji648@yahoo.com>
2026-07-24 13:04:05 +0100

    added interface and method implementation

--- tokenbucket.go
+++ tokenbucket.go
@@ -11,6 +11,18 @@ type TokenBucket struct {
 
 	tokens     int
 	lastRefill time.Time
+
+	clock Clock
+}
+
+type Clock interface {
+	Now() time.Time
+}
+
+type SystemClock struct{}
+
+func (SystemClock) Now() time.Time {
+	return time.Now()
 }
 
 func NewTokenBucket(capacity, refillRate int) (*TokenBucket, error) {
@@ -22,10 +34,12 @@ func NewTokenBucket(capacity, refillRate int) (*TokenBucket, error) {
 		return nil, fmt.Errorf("refill rate must be geater than zero")
 	}
 
+	clock := SystemClock{}
 	return &TokenBucket{
 		capacity:   capacity,
 		refillRate: refillRate,
 		tokens:     capacity,
-		lastRefill: time.Now(),
+		lastRefill: clock.Now(),
+		clock:      clock,
 	}, nil
 }
