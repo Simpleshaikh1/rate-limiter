b8676794351f92a79c54bd782f79f6b8f91aa30b
Toyyib <hamzatbolaji648@yahoo.com>
2026-07-24 11:45:24 +0100

    Token Bucket struct that define the state, immutable and mutable state.  New Token Bucket Instance

--- tokenbucket.go
+++ tokenbucket.go
@@ -1,1 +1,31 @@
 package rate_limiter_token_bucket_leaky_bucket
+
+import (
+	"fmt"
+	"time"
+)
+
+type TokenBucket struct {
+	capacity   int
+	refillRate int
+
+	tokens     int
+	lastRefill time.Time
+}
+
+func NewTokenBucket(capacity, refillRate int) (*TokenBucket, error) {
+	if capacity <= 0 {
+		return nil, fmt.Errorf("capacity must be greater than zero")
+	}
+
+	if refillRate <= 0 {
+		return nil, fmt.Errorf("refill rate must be geater than zero")
+	}
+
+	return &TokenBucket{
+		capacity:   capacity,
+		refillRate: refillRate,
+		tokens:     capacity,
+		lastRefill: time.Now(),
+	}, nil
+}
