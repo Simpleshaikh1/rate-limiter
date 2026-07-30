59ca1f7c50894b38bf9e86ac9090903c38682003
Toyyib <hamzatbolaji648@yahoo.com>
2026-07-30 22:21:03 +0100

    testing redis

--- redisStore/redisstore.go
+++ redisStore/redisstore.go
@@ -1,6 +1,7 @@
 package redisstore
 
 import (
+	"context"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -13,3 +14,31 @@ func New(client *redis.Client) *Store {
 		client: client,
 	}
 }
+
+func (s *Store) Set(
+	ctx context.Context,
+	key string,
+	value string,
+) error {
+	return s.client.Set(ctx, key, value, 0).Err()
+}
+
+func (s *Store) Get(
+	ctx context.Context,
+	key string,
+) (string, error) {
+	return s.client.Get(ctx, key).Result()
+}
+
+//func test() {
+//	ctx := context.Background()
+//
+//	client := redis.NewClient(&redis.Options{
+//		Addr: "localhost:6379",
+//	})
+//
+//	err := client.Ping(ctx).Err()
+//	if err != nil {
+//		panic(err)
+//	}
+//}
