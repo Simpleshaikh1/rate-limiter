ac1cb11b99ac3cb6be0e821d99cbdb657b188a03
Toyyib <hamzatbolaji648@yahoo.com>
2026-07-30 17:15:04 +0100

    test to make sure mutex still protects the map

--- limiter/limiter_test.go
+++ limiter/limiter_test.go
@@ -1,6 +1,7 @@
 package limiter
 
 import (
+	"fmt"
 	"github.com/Simpleshaikh1/rate-limiter/bucket"
 	"sync"
 	"sync/atomic"
@@ -62,5 +63,37 @@ func TestDifferentClientsHaveDifferentBuckets(t *testing.T) {
 	third := l.Allow("bob")
 	if !third.Allowed {
 		t.Fatal("bob should have an independent bucket")
+	}
+}
+
+func TestConcurrentClients(t *testing.T) {
+	cfg, _ := bucket.NewConfig(1000, time.Hour)
+
+	l := New(cfg)
+
+	const workers = 1000
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+
+		go func(id int) {
+			defer wg.Done()
+
+			clientID := fmt.Sprintf("client-%d", id)
+
+			l.Allow(clientID)
+		}(i)
+	}
+
+	wg.Wait()
+
+	if len(l.buckets) != workers {
+		t.Fatalf(
+			"expected %d buckets, got %d",
+			workers,
+			len(l.buckets),
+		)
 	}
 }
